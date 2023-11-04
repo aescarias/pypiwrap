0c6dee498aaa15f22ac648244679ab192a8b6300
Ángel Carias <69056344+aescarias@users.noreply.github.com>
2023-11-04 13:40:38 -0600

    :sparkles: Add host parameter to clients

--- pypiwrap/client.py
+++ pypiwrap/client.py
@@ -5,19 +5,21 @@ import requests
 
 from . import exceptions, objects
 
-JSON_URL = "https://pypi.org/pypi"
-STATS_URL = "https://pypi.org/stats"
-
-SIMPLE_URL = "https://pypi.org/simple"
+PYPI_HOST = "https://pypi.org" 
 SIMPLE_CONTENT_TYPE = "application/vnd.pypi.simple.v1+json"
-
 USER_AGENT = "pypiwrap/1.0.0 (github: aescarias)"
+
+#? The constants below are marked for potential deprecation.
+JSON_URL = f"{PYPI_HOST}/pypi"
+STATS_URL = f"{PYPI_HOST}/stats"
+SIMPLE_URL = f"{PYPI_HOST}/simple"
 
 
 class PyPIClient:
     """Client for the PyPI JSON and Stats API"""
 
-    def __init__(self) -> None:
+    def __init__(self, host=PYPI_HOST) -> None:
+        self.host = host
         self.rest = requests.Session()
         self.rest.headers["User-Agent"] = USER_AGENT
 
@@ -28,20 +30,20 @@ class PyPIClient:
         self.rest.close()
 
     def get_project(self, name: str, version: str | None = None) -> objects.Project:
-        """Gets information about a project or any of its specific releases.
+        """Gets information about a project or any of its releases.
         
         Arguments:
             name (:class:`str`): The name of the project
             
             version (:class:`str`, optional):
-                A specific version of the project. If none specified,
-                the latest is assumed.
+                A version of the project to fetch. If none specified,
+                the latest will be fetched.
         """
         
         if version:
-            rs = self.rest.get(f"{JSON_URL}/{name}/{version}/json")
+            rs = self.rest.get(f"{self.host}/pypi/{name}/{version}/json")
         else:
-            rs = self.rest.get(f"{JSON_URL}/{name}/json")
+            rs = self.rest.get(f"{self.host}/pypi/{name}/json")
         
         exceptions.raise_for_status(rs, {
             404: f"Could not find project or release for '{name}'"
@@ -52,7 +54,7 @@ class PyPIClient:
     def get_stats(self) -> objects.Stats:
         """Gets statistics about PyPI"""
         
-        rs = self.rest.get("https://pypi.org/stats", 
+        rs = self.rest.get(f"{self.host}/stats", 
                            headers={ "Accept": "application/json"})
         exceptions.raise_for_status(rs)
 
@@ -62,7 +64,8 @@ class PyPIClient:
 class SimpleClient:
     """Client for the PyPI Simple API"""
 
-    def __init__(self) -> None:
+    def __init__(self, host: str = PYPI_HOST) -> None:
+        self.host = host
         self.rest = requests.Session()
         self.rest.headers["Accept"] = SIMPLE_CONTENT_TYPE
         self.rest.headers["User-Agent"] = USER_AGENT
@@ -74,16 +77,16 @@ class SimpleClient:
         self.rest.close()
     
     def get_index(self) -> Generator[str, None, None]:
-        """Yields a list of all projects registered on PyPI"""
-        rs = self.rest.get(SIMPLE_URL)
+        """Yields a list of names for all projects registered on this repository"""
+        rs = self.rest.get(f"{self.host}/simple")
         exceptions.raise_for_status(rs)
 
         for project in rs.json()["projects"]:
             yield project["name"]
 
     def get_page(self, project: str) -> objects.ProjectPage:
-        """Gets a specific page for a project"""
-        rs = self.rest.get(f"{SIMPLE_URL}/{project}")
+        """Gets the page for a given project"""
+        rs = self.rest.get(f"{self.host}/simple/{project}")
         exceptions.raise_for_status(rs)
         
         return objects.ProjectPage._from_raw(rs.json())
